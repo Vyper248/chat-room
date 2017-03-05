33c6c616630738a1ab243f77415bb16d311f7ae3
Chris Wilson <vyper374@hotmail.com>
2017-03-05 15:38:23 +0000

    added socket io to frontend

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname+'/public'));
+
+io.on('connection', function(){
+    console.log('Connected via socket.io!');
+});
 
 http.listen(34862, function(){
     console.log("Server started on port 34862");
